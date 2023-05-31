836638cb6899322b8c0a5827bbe6006781216a1f
Elijah Delavar <elijah.delavar@wsu.edu>
2023-05-31 17:15:31 +0000

    Time execution

--- main.c
+++ main.c
@@ -8,22 +8,35 @@ TODO: Description
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "mergeSort.h"
 
 int main(int argc, char const *argv[]) {
-    int arr[] = {3};
-    int size = 1;
+    int size = 1000000;
+    int arr[size];
 
-    printf("Original Array: ");
-    printArray(arr, size);
+    for (int ii = 0; ii < size; ii++) {
+        arr[ii] = ii;
+    }
 
-    int err = sort(arr, size);
+    clock_t t;
+    t = clock();
+    puts("Timer Start");
+
+    // printf("Original Array: ");
+    // printArray(arr, size);
+
+    int err = process(arr, size);
     if (err) {
         return err;
     }
     
-    printf("Sorted Array:   ");
-    printArray(arr, size);
+    // printf("Sorted Array:   ");
+    // printArray(arr, size);
+
+    t = clock() - t;
+    double timeTaken = ((double)t)/CLOCKS_PER_SEC;
+    printf("Timer Stop\nExecuted in %f seconds\n", timeTaken);
 
     return 0;
 }
